96452b79022b4717980697911d60772b53121ca7
STEVE10MR <jm2019063316@virtual.upt.pe>
2024-06-02 15:09:19 -0500

    v0.0.2

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -61,7 +61,7 @@ export default (err, req, res, next) => {
   
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'alfa') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
