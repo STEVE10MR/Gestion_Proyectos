ae8f06a5c05256e55bbe5209789ca06f77d40e48
STEVE10MR <jm2019063316@virtual.upt.pe>
2024-06-05 00:58:10 -0500

    v0.0.3

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,7 +4,7 @@ import authMiddleware from '../middleware/authMiddleware.js';
 const router = express.Router()
 
 router.post('/login',authController.login)
-router.post('/verify-session',authMiddleware,authController.verifySession)
+router.get('/verify-session',authMiddleware,authController.verifySession)
 router.patch('/forgotpassword',authController.forgotPassword)
 router.patch('/resetpassword/:token',authController.resetPassword)
 router.patch('/verification/:token',authController.validateEmailAddress)
